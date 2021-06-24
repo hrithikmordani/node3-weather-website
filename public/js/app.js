0a09b535a01425e3aa5cb8c117f4332360b5a483
hrithikmordani <hrithik.mordani36@nmims.edu.in>
2021-06-24 20:07:22 +0530

    set up app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
     message_one.textContent = 'Loading...'
-    const url = "http://localhost:3000/weather?address=" + location
+    const url = "/weather?address=" + location
     fetch(url).then((response) => {
 
         response.json().then((data) => {
--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require("express")
+const port = process.env.PORT || 3000
 const geocode = require("./utils/geocode")
 const forecast = require("./utils/forecast")
 const hbs = require('hbs')
@@ -8,60 +9,60 @@ console.log(__dirname)
 console.log(__filename)
 
 //define paths for express config
-const publicDir = path.join(__dirname,'../public')
-const viewsDir =  path.join(__dirname,'../templates/views')
-const partialsPath = path.join(__dirname,'../templates/partials')
+const publicDir = path.join(__dirname, '../public')
+const viewsDir = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 
 
 //set up handlebars engine and views location
-app.set('view engine','hbs')
+app.set('view engine', 'hbs')
 app.set('views', viewsDir)
 hbs.registerPartials(partialsPath)
 
 //set up static directory to serve
 app.use(express.static(publicDir))
 
-app.get("",(req,res)=>{
-    res.render('index',{
-        title:'Weather App',
-        name:"Hrithik Mordani"
+app.get("", (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: "Hrithik Mordani"
     })
 })
 
-app.get("/about",(req,res)=>{
-    res.render('about',{
-        title:'About me',
-        name:"Hrithik Mordani"
+app.get("/about", (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: "Hrithik Mordani"
 
     })
 })
 
-app.get("/help",(req,res)=>{
-    res.render('help',{
-        message:"seeking help",
-        title:"Help Page",
-        name:'Hrithik Mordani'
+app.get("/help", (req, res) => {
+    res.render('help', {
+        message: "seeking help",
+        title: "Help Page",
+        name: 'Hrithik Mordani'
     })
 })
 
 
-app.get('/weather',(req,res)=>{
-    if(!req.query.address){
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
         return res.send({
-            error:'You must provide address'
+            error: 'You must provide address'
         })
 
     }
-    geocode(req.query.address,(error,response={})=>{
-        if(error){
+    geocode(req.query.address, (error, response = {}) => {
+        if (error) {
             return res.send({
                 error
             })
 
         }
-        forecast(response.latitude,response.longitude,(error,response_one)=>{
-            if(error){
+        forecast(response.latitude, response.longitude, (error, response_one) => {
+            if (error) {
                 return res.send({
                     error
                 })
@@ -71,19 +72,19 @@ app.get('/weather',(req,res)=>{
                 current_temp: response_one.current_temp,
                 feels_like: response_one.feels_like,
                 location: response.location
-                
+
             })
         })
 
     })
 
-    
+
 })
 
-app.get("/products",(req,res)=>{
-    if(!req.query.search){
+app.get("/products", (req, res) => {
+    if (!req.query.search) {
         return res.send({
-            error:'You must provide search term'
+            error: 'You must provide search term'
         })
     }
     console.log(req.query)
@@ -92,24 +93,23 @@ app.get("/products",(req,res)=>{
     })
 })
 
-app.get('/help/*',(req,res)=>{
-    res.render('error',{
-        error_message:'Help article not found',
-        name:'Hrithik Mordani',
-        title:'404'
+app.get('/help/*', (req, res) => {
+    res.render('error', {
+        error_message: 'Help article not found',
+        name: 'Hrithik Mordani',
+        title: '404'
 
     })
 })
 
-app.get('*',(req,res)=>{
-    res.render('error',{
-        error_message:'Page not found',
-        name:'Hrithik Mordani',
-        title:'404'
+app.get('*', (req, res) => {
+    res.render('error', {
+        error_message: 'Page not found',
+        name: 'Hrithik Mordani',
+        title: '404'
     })
 })
 
-app.listen(3000,()=>{
-    console.log('server is up on port 3000')
+app.listen(port, () => {
+    console.log('server is up on port ' + port)
 })
-
